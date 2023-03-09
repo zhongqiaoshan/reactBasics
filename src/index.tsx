7c2d5e4e23658fdf5fdd5888347eae28afe6337f
zhongqiaoshan <2500349296@qq.com>
2023-03-09 21:09:05 +0800

    '打包配置'

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const root = document.querySelector('#root');
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('BASE_ENV', process.env.BASE_ENV)
+
+if(root) {
+    createRoot(root).render(<App />);
+}
