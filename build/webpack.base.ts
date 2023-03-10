d0713ac9963c07844ad949a09d29e9a50fdfc2d1
zhongqiaoshan <2500349296@qq.com>
2023-03-10 10:59:44 +0800

    feat: 集成代码规范和性能优化

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -1,46 +1,48 @@
-import { Configuration, DefinePlugin } from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import * as dotenv from "dotenv";
-import WebpackBar from 'webpackbar';
+import { Configuration, DefinePlugin } from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import * as dotenv from 'dotenv'
+import WebpackBar from 'webpackbar'
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const path = require("path");
+const path = require('path')
+
 const isDev = process.env.NODE_ENV === 'development' // 是否是开发模式
-console.log("NODE_ENV", process.env.NODE_ENV);
-console.log("BASE_ENV", process.env.BASE_ENV);
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('BASE_ENV', process.env.BASE_ENV)
 
 // 加载配置文件
 const envConfig = dotenv.config({
-  path: path.resolve(__dirname, "../env/.env." + process.env.BASE_ENV),
-});
+  path: path.resolve(__dirname, `../env/.env.${process.env.BASE_ENV}`)
+})
 
-const cssRegex = /\.css$/;
-const sassRegex = /\.(scss|sass)$/;
-const lessRegex = /\.less$/;
+const cssRegex = /\.css$/
+const sassRegex = /\.(scss|sass)$/
+const lessRegex = /\.less$/
 
 const styleLoadersArray = [
-  isDev ? "style-loader" : MiniCssExtractPlugin.loader, // 开发环境使用style-looader,打包模式抽离css
+  isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // 开发环境使用style-looader,打包模式抽离css
   {
-    loader: "css-loader",
+    loader: 'css-loader',
     options: {
       modules: {
-        localIdentName: "[path][name]__[local]--[hash:5]",
-      },
-    },
+        localIdentName: '[path][name]__[local]--[hash:5]'
+      }
+    }
   },
   'postcss-loader'
-];
+]
 
 const baseConfig: Configuration = {
-  entry: path.join(__dirname, "../src/index.tsx"), // 入口文件
+  entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
   // 打包出口文件
   output: {
     filename: 'static/js/[name].[chunkhash:8].js', // // 加上[chunkhash:8]
-    path: path.join(__dirname, "../dist"), // 打包结果输出路径
+    path: path.join(__dirname, '../dist'), // 打包结果输出路径
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
-    publicPath: "/", // 打包后文件的公共前缀路径
+    publicPath: '/', // 打包后文件的公共前缀路径
     // ... 这里自定义输出文件名的方式是，将某些资源发送到指定目录
-    assetModuleFilename: "images/[hash][ext][query]",
+    assetModuleFilename: 'images/[hash][ext][query]'
   },
   // loader 配置
   module: {
@@ -48,120 +50,120 @@ const baseConfig: Configuration = {
       {
         test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
         exclude: /node_modules/,
-        use: ['thread-loader', 'babel-loader'],
+        use: ['thread-loader', 'babel-loader']
       },
       {
-        test: cssRegex, //匹配 css 文件
-        use: styleLoadersArray,
+        test: cssRegex, // 匹配 css 文件
+        use: styleLoadersArray
       },
       {
         test: lessRegex,
         use: [
           ...styleLoadersArray,
           {
-            loader: "less-loader",
+            loader: 'less-loader',
             options: {
               lessOptions: {
                 // 如果要在less中写类型js的语法，需要加这一个配置
-                javascriptEnabled: true,
-              },
-            },
-          },
-        ],
+                javascriptEnabled: true
+              }
+            }
+          }
+        ]
       },
       {
         test: sassRegex,
-        use: [...styleLoadersArray, "sass-loader"],
+        use: [...styleLoadersArray, 'sass-loader']
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i, // 匹配图片文件
-        type: "asset", // type选择asset
+        type: 'asset', // type选择asset
         parser: {
           dataUrlCondition: {
-            maxSize: 20 * 1024, // 小于10kb转base64
-          },
+            maxSize: 20 * 1024 // 小于10kb转base64
+          }
         },
-        generator:{ 
-          filename:'static/images/[name].[contenthash:8][ext]' // 加上[contenthash:8]
-        },
+        generator: {
+          filename: 'static/images/[name].[contenthash:8][ext]' // 加上[contenthash:8]
+        }
       },
       {
         test: /.(woff2?|eot|ttf|otf)$/, // 匹配字体图标文件
-        type: "asset", // type选择asset
+        type: 'asset', // type选择asset
         parser: {
           dataUrlCondition: {
-            maxSize: 10 * 1024, // 小于10kb转base64
-          },
+            maxSize: 10 * 1024 // 小于10kb转base64
+          }
         },
-        generator:{ 
-          filename:'static/fonts/[name].[contenthash:8][ext]', // 加上[contenthash:8]
-        },
+        generator: {
+          filename: 'static/fonts/[name].[contenthash:8][ext]' // 加上[contenthash:8]
+        }
       },
       {
         test: /.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
-        type: "asset", // type选择asset
+        type: 'asset', // type选择asset
         parser: {
           dataUrlCondition: {
-            maxSize: 10 * 1024, // 小于10kb转base64
-          },
+            maxSize: 10 * 1024 // 小于10kb转base64
+          }
         },
-        generator:{ 
-          filename:'static/media/[name].[contenthash:8][ext]', // 加上[contenthash:8]
-        },
+        generator: {
+          filename: 'static/media/[name].[contenthash:8][ext]' // 加上[contenthash:8]
+        }
       },
       {
         // 匹配json文件
         test: /\.json$/,
-        type: "asset/resource", // 将json文件视为文件类型
+        type: 'asset/resource', // 将json文件视为文件类型
         generator: {
           // 这里专门针对json文件的处理
-          filename: "static/json/[name].[hash][ext][query]",
-        },
-      },
-    ],
+          filename: 'static/json/[name].[hash][ext][query]'
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".less", ".css"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.less', '.css'],
     // 别名需要配置两个地方，这里和 tsconfig.json
     alias: {
-      "@": path.join(__dirname, "../src"),
-    },
+      '@': path.join(__dirname, '../src')
+    }
     // modules: [path.resolve(__dirname, "../node_modules")], // 查找第三方模块只在本项目的node_modules中查找
   },
   // plugins 的配置
   plugins: [
     new HtmlWebpackPlugin({
-      title: "webpack5-react-ts",
-      filename: "index.html",
+      title: 'webpack5-react-ts',
+      filename: 'index.html',
       // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
-      template: path.join(__dirname, "../public/index.html"),
+      template: path.join(__dirname, '../public/index.html'),
       inject: true, // 自动注入静态资源
       hash: true,
       cache: false,
       // 压缩html资源
       minify: {
         removeAttributeQuotes: true,
-        collapseWhitespace: true, //去空格
+        collapseWhitespace: true, // 去空格
         removeComments: true, // 去注释
         minifyJS: true, // 在脚本元素和事件属性中缩小JavaScript(使用UglifyJS)
-        minifyCSS: true, // 缩小CSS样式元素和样式属性
+        minifyCSS: true // 缩小CSS样式元素和样式属性
       },
-      nodeModules: path.resolve(__dirname, "../node_modules"),
+      nodeModules: path.resolve(__dirname, '../node_modules')
     }),
     new DefinePlugin({
-      "process.env": JSON.stringify(envConfig.parsed),
-      "process.env.BASE_ENV": JSON.stringify(process.env.BASE_ENV),
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      'process.env': JSON.stringify(envConfig.parsed),
+      'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
     new WebpackBar({
-      color: "#85d",  // 默认green，进度条颜色支持HEX
-      basic: false,   // 默认true，启用一个简单的日志报告器
-      profile:false,  // 默认false，启用探查器。
+      color: '#85d', // 默认green，进度条颜色支持HEX
+      basic: false, // 默认true，启用一个简单的日志报告器
+      profile: false // 默认false，启用探查器。
     })
   ],
   cache: {
-    type: 'filesystem', // 使用文件缓存
-  },
-};
+    type: 'filesystem' // 使用文件缓存
+  }
+}
 
-export default baseConfig;
+export default baseConfig
--- build/webpack.prod.ts
+++ build/webpack.prod.ts
@@ -1,19 +1,16 @@
-import path from "path";
-import { Configuration } from "webpack";
-import { merge } from "webpack-merge";
-import CopyPlugin from "copy-webpack-plugin";
-import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
-import TerserPlugin from "terser-webpack-plugin";
-import baseConfig from "./webpack.base";
+import path from 'path'
+import { Configuration } from 'webpack'
+import { merge } from 'webpack-merge'
+import CopyPlugin from 'copy-webpack-plugin'
+import baseConfig from './webpack.base'
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const globAll = require('glob-all')
 const { PurgeCSSPlugin } = require('purgecss-webpack-plugin')
-const glob = require('glob')
-const CompressionPlugin  = require('compression-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin')
 
 const prodConfig: Configuration = merge(baseConfig, {
-  mode: "production", // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
+  mode: 'production', // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
   /**
    * 打包环境推荐：none(就是不配置devtool选项了，不是配置devtool: 'none')
    * ● none话调试只能看到编译后的代码,也不会泄露源代码,打包速度也会比较快。
@@ -23,17 +20,17 @@ const prodConfig: Configuration = merge(baseConfig, {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "../public"), // 复制public下文件
-          to: path.resolve(__dirname, "../dist"), // 复制到dist目录中
-          filter: (source) => !source.includes("index.html"), // 忽略index.html
-        },
-      ],
+          from: path.resolve(__dirname, '../public'), // 复制public下文件
+          to: path.resolve(__dirname, '../dist'), // 复制到dist目录中
+          filter: source => !source.includes('index.html') // 忽略index.html
+        }
+      ]
     }),
     // 抽离css插件
     new MiniCssExtractPlugin({
-      filename: "static/css/[name].[contenthash:8].css", // 加上[contenthash:8]
+      filename: 'static/css/[name].[contenthash:8].css' // 加上[contenthash:8]
     }),
-     // 清理无用css，检测src下所有tsx文件和public下index.html中使用的类名和id和标签名称
+    // 清理无用css，检测src下所有tsx文件和public下index.html中使用的类名和id和标签名称
     // 只打包这些文件中用到的样式
     new PurgeCSSPlugin({
       paths: globAll.sync(
@@ -44,7 +41,7 @@ const prodConfig: Configuration = merge(baseConfig, {
       ),
       // 用 only 来指定 purgecss-webpack-plugin 的入口
       // https://github.com/FullHuman/purgecss/tree/main/packages/purgecss-webpack-plugin
-      only: ["dist"],
+      only: ['dist'],
       safelist: {
         standard: [/^ant-/] // 过滤以ant-开头的类名，哪怕没用到也不删除
       }
@@ -52,11 +49,11 @@ const prodConfig: Configuration = merge(baseConfig, {
     // 打包时生成gzip文件
     new CompressionPlugin({
       test: /\.(js|css)$/, // 只生成css,js压缩文件
-      filename: "[path][base].gz", // 文件命名
-      algorithm: "gzip", // 压缩格式,默认是gzip
+      filename: '[path][base].gz', // 文件命名
+      algorithm: 'gzip', // 压缩格式,默认是gzip
       threshold: 10240, // 只有大小大于该值的资源会被处理。默认值是 10k
-      minRatio: 0.8, // 压缩率,默认值是 0.8
-    }),
+      minRatio: 0.8 // 压缩率,默认值是 0.8
+    })
   ],
   optimization: {
     splitChunks: {
@@ -65,27 +62,27 @@ const prodConfig: Configuration = merge(baseConfig, {
         vendors: {
           // 提取node_modules代码
           test: /node_modules/, // 只匹配node_modules里面的模块
-          name: "vendors", // 提取文件命名为vendors,js后缀和chunkhash会自动加
+          name: 'vendors', // 提取文件命名为vendors,js后缀和chunkhash会自动加
           minChunks: 1, // 只要使用一次就提取出来
-          chunks: "initial", // 只提取初始化就能获取到的模块,不管异步的
+          chunks: 'initial', // 只提取初始化就能获取到的模块,不管异步的
           minSize: 0, // 提取代码体积大于0就提取出来
-          priority: 1, // 提取优先级为1
+          priority: 1 // 提取优先级为1
         },
         commons: {
           // 提取页面公共代码
-          name: "commons", // 提取文件命名为commons
+          name: 'commons', // 提取文件命名为commons
           minChunks: 2, // 只要使用两次就提取出来
-          chunks: "initial", // 只提取初始化就能获取到的模块,不管异步的
-          minSize: 0, // 提取代码体积大于0就提取出来
-        },
-      },
-    },
+          chunks: 'initial', // 只提取初始化就能获取到的模块,不管异步的
+          minSize: 0 // 提取代码体积大于0就提取出来
+        }
+      }
+    }
   },
   performance: {
     hints: false,
     maxAssetSize: 4000000, // 整数类型（以字节为单位）
-    maxEntrypointSize: 5000000, // 整数类型（以字节为单位）
-  },
-});
+    maxEntrypointSize: 5000000 // 整数类型（以字节为单位）
+  }
+})
 
-export default prodConfig;
+export default prodConfig
